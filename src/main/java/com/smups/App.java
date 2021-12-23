031c0ad65d05fa39e47c5949a62bf81dccff6646
Raúl Wolters <56522208+smups@users.noreply.github.com>
2021-12-23 17:54:47 +0100

    formatting gefixt

--- src/main/java/com/smups/App.java
+++ src/main/java/com/smups/App.java
@@ -1,13 +1,48 @@
 package com.smups;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.smups.codenightly.Conventions;
+import com.smups.codenightly.request.Bericht;
+
+public class App {
+    //Port voor de server
+    public static final int PORT = Conventions.CODENIGHTLY_PORT;
+
+    public static void main(String[] args) {
+        System.out.println("Starting codedusk v1.0 server...");
+        //Probeer de server te starten
+        ServerSocket server;
+        try {
+            server = new ServerSocket(PORT);
+        } catch (Exception e) {
+            System.out.printf("[FATAL ERROR] - could not open server on port %d\n",
+                PORT
+            );
+            return; //Fatal exception - sluit het programma
+        }
+
+        //We hebben nu een server -> open een verbinding met een client
+        System.out.printf("[Codedusk] server is listening on port %d...\n",
+            PORT
+        );
+
+        while (true) {
+            try {
+                Socket socket = server.accept();
+                //Grab input from socket and turn bytes into a Bericht
+                ObjectInputStream objstr = new ObjectInputStream(socket.getInputStream());
+                Bericht bericht = (Bericht) objstr.readObject();
+
+                //Voer het bericht aan het Handler-monster lol
+                //TODO
+
+            } catch (Exception e) {
+                System.out.println("[Codedusk] server could not process request");
+                e.printStackTrace();
+            }
+        }
     }
 }
