74c33f4ef5b8c03befa4f5b15e685e64ac0eca3c
Raúl Wolters <56522208+smups@users.noreply.github.com>
2021-12-23 15:14:12 +0100

    maven project gestart

---
+++ src/main/java/com/smups/App.java
@@ -0,0 +1,13 @@
+package com.smups;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
